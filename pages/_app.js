d60841b13c29958045040c47b82a5f181a54b960
chu-duy <ninjavnp@gmail.com>
2021-10-16 16:56:00 +0700

    Fix dynamic import for bootstrap js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
 
-// import 'bootstrap/dist/js/bootstrap'
+import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap')
+    })
+    return <Component {...pageProps} />
 }
 
 export default MyApp
