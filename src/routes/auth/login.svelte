dfc957bd7fc3f38624ca7701556cdd64a61ab6a8
BpHana <dorewan2009cn@gmail.com>
2022-03-03 08:38:23 +0700

    Update login.svelte

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -30,6 +30,7 @@
                 processing = false;
                 if(response.status == 200){
                     error_status = false;
+                    goto("/");
                 }else{
                     console.log(response)
                     error_status = true;
@@ -90,10 +91,7 @@
                                             class="text-white bg-blue-500 text-sm font-bold uppercase px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                                             type="submit"
                                             disabled>
-                                            <svg class="animate-spin text-white h-6 w-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                            </svg>
+                                            <icon class="fa-solid fa-spinner fa-spin-pulse fa-xl mr-1"></icon>
                                             Processing..
                                         </button>
                                         {:else}
